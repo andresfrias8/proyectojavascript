5b19f62db2165fc8eaa6db6859fe6ebdaf5ff76f
andresfrias8 <andresfrias8@gmail.com>
2024-01-13 18:51:54 -0300

    segnda grabacion

--- data.js
+++ data.js
@@ -30,8 +30,31 @@ if(usuario == "andres" && intentos <=3){
 
 }while(identificar)
 
+/* function saludar (){
+    let usuario
+    console.log("hola " + usuario) 
+}
+saludar()
+*/
 
+/*
+function login(){
+    let usuario = prompt("ingresa tu usuario: ")
+    if(usuario !== ""){
+        alert("error, dato ,mal ingresado")
+    }
+}
+login ()
+*/
 
+function sumar (numA,numB){
+    numA = parseInt(prompt("ingrese primer numero que desea sumar"))
+    numB = parseInt(prompt("ingrese segundo numero que desea sumar"))
+
+    let resultado = numA + numB
+    console.log("el resultado es: " + resultado)
+}
+sumar( )
 /*
 
 const nombre = prompt ("como es tu nombre?")
@@ -39,3 +62,20 @@ console.log ("nombre del usuario " + nombre)
 alert ("Bienvenido " + nombre)
 
 */
+
+const IVA = 1.21
+
+function calcularIva(importe){
+    if(parseFloat(importe)){
+        let resultado = importe * IVA
+        alert("el importe con IVA es: " + resultado)
+    }else{
+        alert("eso no es un numero")
+    }
+}
+function calcularPrecioFinal(){
+    let precioDelProducto = prompt("ingrese el importe del producto: ")
+    calcularIva(precioDelProducto)
+}
+
+calcularPrecioFinal()
